b657e7bf1869a4ea28a23bdd45bd2399b63ecf67
Brian <brianbaklaursen@gmail.com>
2014-04-01 21:59:10 +0200

    Addition of mongoose rental scheme

---
+++ app/models/rental.js
@@ -0,0 +1,65 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+
+/**
+ * Rental Schema
+ */
+var RentalSchema = new Schema({
+    user: {
+        type: Schema.ObjectId,
+        ref: 'User'
+    },
+    price: {
+        type: String,
+        required: true
+    },
+    occupied: [
+        {fromDate: Date, toDate: Date, rent: {
+            type: Schema.ObjectId,
+            ref: 'Rent'
+        }}
+    ],
+    period: {
+        fromDate: Date,
+        toDate: Date
+    },
+    location: {
+        address: String,
+        city: String,
+        country: String
+    },
+    description: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    storageType: {
+        type: String,
+        default: '',
+        trim: true
+    }
+});
+
+/**
+ * Validations
+ */
+RentalSchema.path('description').validate(function(title) {
+    return title.length;
+}, 'Title cannot be blank');
+
+/**
+ * Statics
+ */
+RentalSchema.statics.load = function(id, cb) {
+    this.findOne({
+        _id: id
+    }).populate('user', 'name username').exec(cb);
+};
+
+mongoose.model('Rental', RentalSchema);
