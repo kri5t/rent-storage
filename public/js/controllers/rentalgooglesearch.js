39abd6993a4ced143e7fb7f94b8170527cba23e3
Brian <brianbaklaursen@gmail.com>
2014-04-08 19:06:09 +0200

    Added google maps directive

---
+++ public/js/controllers/rentalgooglesearch.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('mean.articles').controller('RentalSearchController', ['$scope', '$stateParams', '$location', 'Global', 'google-maps', function ($scope, $stateParams, $location, Global) {
+    $scope.global = Global;
+
+  // $scope.gPlace;
+}]);
--- public/js/directives.js
+++ public/js/directives.js
@@ -1,1 +1,20 @@
 'use strict';
+
+angular.module('mean.articles').directive('googleplace', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, model) {
+            var options = {
+                types: [],
+                componentRestrictions: {}
+            };
+            scope.gPlace = new google.maps.places.Autocomplete(element[0], options);
+
+            google.maps.event.addListener(scope.gPlace, 'place_changed', function() {
+                scope.$apply(function() {
+                    model.$setViewValue(element.val());
+                });
+            });
+        }
+    };
+});
