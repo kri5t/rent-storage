75b8165e5d25750a546adc98a133384771d65c82
Brian <brianbaklaursen@gmail.com>
2014-04-08 20:55:01 +0200

    Added google maps directive

--- public/js/controllers/rentalgooglesearch.js
+++ public/js/controllers/rentalgooglesearch.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('mean.articles').controller('RentalSearchController', ['$scope', '$stateParams', '$location', 'Global', 'google-maps', function ($scope, $stateParams, $location, Global) {
+angular.module('mean.articles').controller('RentalSearchController', ['$scope', '$stateParams', '$location', 'Global', function ($scope, $stateParams, $location, Global) {
     $scope.global = Global;
 
   // $scope.gPlace;
