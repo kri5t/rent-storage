866960bc25446c80161415a8263792eabf68c992
Kristian Barrett <kbarrett-86@hotmail.com>
2014-04-08 22:04:54 +0200

    Ændret lidt i booking

--- public/js/controllers/booking.js
+++ public/js/controllers/booking.js
@@ -1,60 +1,64 @@
 'use strict';
 
-angular.module('mean.booking').controller('BookingsController', ['$scope', '$location', 'Bookings', function($scope, $location, Bookings){
+angular.module('mean.booking')
+.controller('BookingsController', ['$scope', '$location', 'Bookings', function($scope, $location, Bookings){
 	$scope.go = function ( path ) {
 		$location.path( path );
 	};
 	$scope.create = function() {
+		console.log($scope);
+		console.log($location);
+		console.log(Bookings);
 		var booking = new Bookings({
-			to: this.to,
-			from: this.from
+			to: this.dt,
+			from: this.dt
 		});
+		console.log(booking);
 		booking.$save(function(response) {
 			$location.path('/bookings/' + response._id);
 		});
 
-		this.to = new Date();
-		this.from = new Date();
+//		this.to = new Date();
+//		this.from = new Date();
 	};
 }])
-	.controller('DatetimeController', ['$scope', function($scope) {
-		$scope.today = function() {
-			$scope.dt = new Date();
-		};
-		$scope.today();
+.controller('DatetimeController', ['$scope', function($scope) {
+	$scope.today = function() {
+		$scope.dt = new Date();
+	};
+	$scope.today();
 
-		$scope.showWeeks = true;
-		$scope.toggleWeeks = function () {
-			$scope.showWeeks = ! $scope.showWeeks;
-		};
+	$scope.showWeeks = true;
+	$scope.toggleWeeks = function () {
+		$scope.showWeeks = ! $scope.showWeeks;
+	};
 
-		$scope.clear = function () {
-			$scope.dt = null;
-		};
+	$scope.clear = function () {
+		$scope.dt = null;
+	};
 
-// Disable weekend selection
-		$scope.disabled = function(date, mode) {
-			return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
-		};
+	// Disable weekend selection
+	$scope.disabled = function(date, mode) {
+		return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+	};
 
-		$scope.toggleMin = function() {
-			$scope.minDate = ( $scope.minDate ) ? null : new Date();
-		};
-		$scope.toggleMin();
+	$scope.toggleMin = function() {
+		$scope.minDate = ( $scope.minDate ) ? null : new Date();
+	};
+	$scope.toggleMin();
 
-		$scope.open = function($event) {
-			$event.preventDefault();
-			$event.stopPropagation();
+	$scope.open = function($event) {
+		$event.preventDefault();
+		$event.stopPropagation();
 
-			$scope.opened = true;
-		};
+		$scope.opened = true;
+	};
 
-		$scope.dateOptions = {
-			'year-format': 'yy',
-			'starting-day': 1
-		};
+	$scope.dateOptions = {
+		'year-format': 'yy',
+		'starting-day': 1
+	};
 
-		$scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'shortDate'];
-		$scope.format = $scope.formats[0];
-	}]);
-;
+	$scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'shortDate'];
+	$scope.format = $scope.formats[0];
+}]);
